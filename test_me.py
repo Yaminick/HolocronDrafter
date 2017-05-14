9c277b54e89934cb98326d697edf4a8c9a2ab537
ncorsopassaro <nick.corso-passaro@ipsoft.com>
2017-05-14 06:11:48 -0400

    ok and now we fix the build

--- test_me.py
+++ test_me.py
@@ -1,13 +1,13 @@
 import pytest
 
 @pytest.mark.parametrize('name, left, right', [['foo', 'a', 'a'],
-                                               ['bar', 'a', 'b'],
+                                               ['bar', 'c', 'c'],
                                                ['baz', 'b', 'b']])
 def test_me(name, left, right):
     assert left == right, name
 
 @pytest.mark.parametrize('name, left, right', [['foo', 'x', 'x'],
                                                ['bar', 'y', 'y'],
-                                               ['baz', 'x', 'z']])
+                                               ['baz', 'z', 'z']])
 def test_me_next(name, left, right):
     assert left == right, name
